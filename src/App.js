c9f379090acef271e628aac33d0142d9fc56c6be
Austin <497133212@qq.com>
2020-08-06 20:01:20 +0800

    [ToDoList][Austin] refactor : refactor ths style

--- src/App.js
+++ src/App.js
@@ -5,51 +5,55 @@ import {HashRouter as Router, Link, Route} from "react-router-dom";
 import CompletedTodoLContainer from "./containers/CompletedTodo";
 import TodoListContainer from "./containers/TodoListContainer";
 import {Layout, Menu} from 'antd';
-import {UserOutlined, VideoCameraOutlined,} from '@ant-design/icons';
+import WeiboCircleOutlined from "@ant-design/icons/es/icons/WeiboCircleOutlined";
+import QqOutlined from "@ant-design/icons/es/icons/QqOutlined";
 
 const {Header, Footer, Sider, Content} = Layout;
 
 function App() {
 
     return (
-        <Router>
-            <Layout>
-                <Sider trigger={null} collapsible collapsed>
-                    <div className="logo"/>
-                    <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-                        <Menu.Item key="1" icon={<UserOutlined/>}>
-                            <Link to="/">TodoList</Link>
-                        </Menu.Item>
-                        <Menu.Item key="2" icon={<VideoCameraOutlined/>}>
-                            <Link to="/completedTodoList">CompletedTodoList</Link>
-                        </Menu.Item>
-                    </Menu>
-                </Sider>
-                <Layout className="site-layout">
-                    <Header className="site-layout-background" style={{padding: 0}}>
-                        <h1>TodoList</h1>
-                    </Header>
-                    <Content
-                        className="site-layout-background"
-                        style={{
-                            margin: '24px 16px',
-                            padding: 24,
-                            minHeight: 280,
-                        }}
-                    >
-                        <Route path="/completedTodoList" component={CompletedTodoLContainer}/>
-                        <Route exact path={"/"} render={() => {
-                            return (
-                                <div>
-                                    <TodoFormContainer/>
-                                    <TodoListContainer/>
-                                </div>
-                            )
-                        }}/>
-                    </Content>
+        <div className="body-layout">
+            <Router>
+                <Layout className="body-layout">
+                    <Sider trigger={null} collapsible collapsed>
+                        <div className="left-layout">
+                            <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+                                <Menu.Item key="1" icon={<WeiboCircleOutlined/>}>
+                                    <Link to="/">TodoList</Link>
+                                </Menu.Item>
+                                <Menu.Item key="2" icon={<QqOutlined/>}>
+                                    <Link to="/completedTodoList">CompletedTodoList</Link>
+                                </Menu.Item>
+                            </Menu>
+                        </div>
+                    </Sider>
+                    <Layout className="site-layout">
+                        <Header theme="dark" style={{padding: 0}}>
+                            <h1 className="Logo">TodoList</h1>
+                        </Header>
+                        <Content
+                            className="site-layout-background"
+                            style={{
+                                margin: '24px 16px',
+                                padding: 24,
+                                minHeight: 280,
+                            }}
+                        >
+                            <Route path="/completedTodoList" component={CompletedTodoLContainer}/>
+                            <Route exact path={"/"} render={() => {
+                                return (
+                                    <div>
+                                        <TodoFormContainer/>
+                                        <TodoListContainer/>
+                                    </div>
+                                )
+                            }}/>
+                        </Content>
+                    </Layout>
                 </Layout>
-            </Layout>
-        </Router>
+            </Router>
+        </div>
     );
 }
 
